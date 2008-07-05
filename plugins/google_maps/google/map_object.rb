bbd72b906a319cf7a04257179c17df53e6912379
Guilio Karadanais <karadanais@gmail.com>
2008-07-05 14:16:20 +0200

    Added new map click logic, extended MapObject#listen_to

--- plugins/google_maps/google/map_object.rb
+++ plugins/google_maps/google/map_object.rb
@@ -8,31 +8,34 @@ module Google
   
     # Listens for events on the client.
     # 
-    # :event:: => Required. The event that should be listened to.
-    # :on::  => Optional. The object on which the event occurs, defaulted to +JavascriptObject#var+
-    # :with::  => Optional. Arguments that are passed along when the event is fired, these will also be yielded to the supplied block.
+    # :event::       => Required. The event that should be listened to.
+    # :on::          => Optional. The object on which the event occurs, defaulted to +JavascriptObject#var+
+    # :with::        => Optional. Arguments that are passed along when the event is fired, these will also be yielded to the supplied block.
+    # :yield_order:: => Optional. The order in which the +with+ options should be yielded to the ruby block.
     #
     # A JavascriptGenerator along with the +with+ option will be yielded to the block.
-    #
-    #   map.listen_to :event => :click, :with => [:overlay, :location] do |script, overlay, location|
-    #     script.alert('hello')
-    #     map.open_info_window(:at => location, :text => 'A window is open!')
-    #     # other code that will occur when this event happens...
-    #   end
     def listen_to(options = {})
       options.default! :on => self.var, :with => []            
-      options.assert_valid_keys :event, :on, :with
+      options.assert_valid_keys :event, :on, :with, :yield_order
       
       with_arguments = options[:with].arify
       js_arguments = with_arguments.join(', ')
-
+      
+      yield_args = if options[:yield_order]
+                     options[:yield_order]
+                   else
+                     with_arguments
+                   end
+      
+      yield_args = self.script.arify + yield_args
+             
       # Wrap the GEvent closure in a method to prevent the non-closure bug of javascript and google maps.
       wrap_method = "#{self.var}_#{options[:event]}(#{self.var})"
 
       self << "function #{wrap_method}{"
       self << "GEvent.addListener(#{options[:on]}, \"#{options[:event]}\", function(#{js_arguments}) {"
-
-      yield *(self.script.arify + with_arguments)
+      
+      yield *yield_args
 
       self <<  "});"
       self << "}"
