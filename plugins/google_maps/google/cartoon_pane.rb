f7d6843e680845565041d4504f92990e3a1a7ace
ryan lemmer <ryanlemmer@gmail.com>
2008-08-23 13:56:22 +0200

    CartoonPane initialiser => take options

--- plugins/google_maps/google/cartoon_pane.rb
+++ plugins/google_maps/google/cartoon_pane.rb
@@ -2,10 +2,9 @@ module Google
 
   class CartoonPane < Pane
 
-    def initialize(text)
-      options = {:var => 'cartoon_pane', 
-                 :text => "<h3 style='margin: 5px; padding: 5px;'>#{text}</h3>"}
-
+    def initialize(text, options = {})
+      options[:text] = "<h3 style='margin: 5px; padding: 5px;'>#{text}</h3>"
+      options[:var] ||= 'cartoon_pane'
       super options
     end
 
