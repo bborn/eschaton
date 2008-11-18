b48977c9b6de5c4f513d3f3540b3d55b055a6f0b
Guilio Karadanais <karadanais@gmail.com>
2008-11-18 21:00:06 +0200

    More extraction

--- slices/quiver_core/object.rb
+++ slices/quiver_core/object.rb
@@ -1,9 +1,5 @@
 class Object
-  
-  def is_options_hash?
-    self.is_a?(Hash)
-  end
-  
+    
   def arify
     if self.is_a?(Array)
       self
@@ -25,26 +21,10 @@ class Object
     instance_variable_get("@#{name}")
   end
   
-  def self.cached_attr_reader(name, &block)
-    class_eval do 
-      define_method(name) do
-        assign_field_once(name, &block.bind(self)) #bind the block to self so that it can see (and use) the class instance's binding
-      end
-    end
-  end
-  
   def in?(*values)
     values.flatten.include?(self)
   end
-  
-  def or_minimum_of(min)
-    if self <= min
-      min
-    else
-      self
-    end
-  end
-  
+    
   def quote
     self.to_s.quote
   end
