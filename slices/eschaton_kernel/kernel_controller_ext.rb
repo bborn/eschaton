d14c618faac4e24cdc2cb00a247abed943d5c944
Guilio Karadanais <karadanais@gmail.com>
2008-11-25 14:40:56 +0200

    Removed quiver_core related method

--- slices/eschaton_kernel/kernel_controller_ext.rb
+++ slices/eschaton_kernel/kernel_controller_ext.rb
@@ -6,10 +6,4 @@ module KernelControllerExt
     end
   end
 
-  def presentation_model(model_name)
-    run_javascript do |script|
-      yield model_name.presentation_modelify.new(script)
-    end
-  end    
-
 end
